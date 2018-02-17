12879460fd2fc49cf9617dbc113a08e9fdfb4eef
jcant <jcant.gm@gmail.com>
2018-02-17 19:43:12 +0200

    HW51 - file utils - interface version

--- HW51FileUtils/src/com/gmail/gm/jcant/FileUtils.java
+++ HW51FileUtils/src/com/gmail/gm/jcant/FileUtils.java
@@ -4,16 +4,93 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Date;
 
 public class FileUtils {
 
-	public static void fileCopy(File from, File to) throws IOException {
+	public static void copy(File from, File to) throws IOException {
+
+		Date start = new Date();
+
+		fileSearch(from, f -> {
+
+			if (f.isFile()) {
+				File fileTo = new File(to,
+						f.getAbsolutePath().substring(from.getAbsolutePath().length(), f.getAbsolutePath().length()));
+
+				File path = new File(fileTo.getAbsolutePath().substring(0,
+						fileTo.getAbsolutePath().length() - fileTo.getName().length()));
+
+				path.mkdirs();
+
+				try {
+					fileCopy(f, new File(to, f.getAbsolutePath().substring(from.getAbsolutePath().length(),
+							f.getAbsolutePath().length())));
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
+			}
+		});
+
+		System.out.println("100%");
+
+		Date end = new Date();
+
+		System.out.println("Time in msec: " + (end.getTime() - start.getTime()));
+	}
+
+	public static void delete(File file) throws IOException {
+
+		Date start = new Date();
+
+		fileSearch(file, f -> f.delete());
+
+		Date end = new Date();
+
+		System.out.println("Time in msec: " + (end.getTime() - start.getTime()));
+	}
+
+	public static long getSize(File file) {
+		long[] result = new long[1];
+
+		fileSearch(file, f -> {
+			if (f.isFile()) {
+				result[0] = result[0] + f.length();
+			}
+		});
+		return result[0];
+	}
+
+	
+	//recursion method to run through all files and directories
+	private static void fileSearch(File startPoint, FileOperation fo) {
+
+		if (startPoint.isFile()) {
+
+			fo.fileOperation(startPoint);
+			return;
+
+		} else {
+
+			File[] files = startPoint.listFiles();
+
+			for (File file : files) {
+
+				fileSearch(file, fo);
+			}
+
+			fo.fileOperation(startPoint);
+			return;
+		}
+
+	}
+
+	private static void fileCopy(File from, File to) throws IOException {
 		long fileSize = from.length() / 4;
 		int buffSize = 0;
-		if (fileSize > 1024 * 1024 *10) {
-			buffSize = 1024 * 1024 *10;
+		if (fileSize > 1024 * 1024 * 10) {
+			buffSize = 1024 * 1024 * 10;
 		} else if (fileSize < 1024) {
 			buffSize = 1024;
 		} else {
@@ -36,63 +113,7 @@ public class FileUtils {
 		}
 	}
 
-	public static void copy(File from, File to) throws IOException {
-
-		Date start = new Date();
-		
-		File[] files = fileSearch(from);
-
-		for (int i = 0; i < files.length; i++) {
-
-			System.out.println("progress: " + (100 * i / files.length) + "%");
-
-			File fileTo = new File(to, files[i].getAbsolutePath().substring(from.getAbsolutePath().length(),
-					files[i].getAbsolutePath().length()));
-
-			File path = new File(fileTo.getAbsolutePath().substring(0,
-					fileTo.getAbsolutePath().length() - fileTo.getName().length()));
-
-			path.mkdirs();
-
-			fileCopy(files[i], new File(to, files[i].getAbsolutePath().substring(from.getAbsolutePath().length(),
-					files[i].getAbsolutePath().length())));
-		}
-
-		System.out.println("100%");
-		
-		Date end = new Date();
-		
-		System.out.println("Time in msec: "+(end.getTime() - start.getTime()));
-	}
-
-	public static File[] fileSearch(File startPoint) {
-
-		File[] result = new File[0];
-
-		if (startPoint.isFile()) {
-
-			result = new File[] { startPoint };
-
-			return result;
-
-		} else {
-
-			File[] files = startPoint.listFiles();
-
-			for (File file : files) {
-
-				File[] tmp = fileSearch(file);
-				File[] newArr = Arrays.copyOf(result, result.length + tmp.length);
-				System.arraycopy(tmp, 0, newArr, result.length, tmp.length);
-				result = newArr;
-			}
-
-			return result;
-		}
-
-	}
-
-	public static void printFileInfo(File file) {
+	private static void printFileInfo(File file) {
 		System.out.println(((file.isFile()) ? ("file") : ("<DIR>")) + "\t name: " + file.getAbsolutePath());
 	}
 }
--- HW51FileUtils/src/com/gmail/gm/jcant/Main.java
+++ HW51FileUtils/src/com/gmail/gm/jcant/Main.java
@@ -8,16 +8,27 @@ public class Main {
 
 	public static void main(String[] args) {
 
+//		File from = new File("/home/jcant/JC/Graphics");
+//		File to = new File("/home/jcant/TMP/222");
+//
+//		try {
+//			FileUtils.copy(from, to);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
-		 File from = new File("/home/jcant/Видео/Танцы");
-		 File to = new File("/media/jcant/A00012170011F54E/TMP");
+//		File delFile = new File("/home/jcant/TMP/222");
+//		try {
+//			FileUtils.delete(delFile);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
-		 try {
-		 FileUtils.copy(from, to);
-		 } catch (IOException e) {
-		 // TODO Auto-generated catch block
-		 e.printStackTrace();
-		 }
+		//get size:
+		File file = new File("/home/jcant/JC/Graphics");
+		System.out.println("Size of "+file+ " is "+ FileUtils.getSize(file)+" bytes");
 
 	}
 
