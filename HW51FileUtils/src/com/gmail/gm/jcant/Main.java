71957853dfd48979a1b181edf1ea3cb5b33bcd57
jcant <jcant.gm@gmail.com>
2018-02-18 09:08:36 +0200

    HW51 - file utils (3)

--- HW51FileUtils/src/com/gmail/gm/jcant/Main.java
+++ HW51FileUtils/src/com/gmail/gm/jcant/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		File from = new File("/home/jcant/JC/Graphics/моя");
-		File to = new File("/home/jcant/TMP/222");
+		File from = new File("/home/Graphics");
+		File to = new File("/home/TMP/222");
 
 		try {
 			FileUtils.copy(from, to, "cdr", "jpg");
