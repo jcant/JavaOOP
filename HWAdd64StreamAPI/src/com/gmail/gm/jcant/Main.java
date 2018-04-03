3b16273e1a891ba6ce8e4a018ed7bab831c3811a
jcant <jcant.gm@gmail.com>
2018-04-03 20:42:04 +0300

    HWAdd63 - stream API - max number from file - done

---
+++ HWAdd64StreamAPI/src/com/gmail/gm/jcant/Main.java
@@ -0,0 +1,28 @@
+package com.gmail.gm.jcant;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int result = 0;
+
+		try {
+			result = Files.lines(Paths.get("numbers.txt"))
+					.flatMap(n -> Arrays.stream(n.replaceAll("[^\\d]{1,}", " ").split(" ")).map(String::trim))
+					.map(n -> Integer.parseInt(n))
+					.max((n, m) -> n - m)
+					.get();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(result);
+
+	}
+}
